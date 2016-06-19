6a8ceef4d16e75e6d4938860749563f6db1290eb
icwong <icwong@sfu.ca>
2016-06-18 17:27:14 -0700

    Added scss Added bourbon Added navigation bar

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+         
+         
+  devise :zxcvbnable
+  # Optionally add more weak words to check against:
+  def weak_words
+    ['mysitename', self.email, 'password', 'coop']
+  end
+end
